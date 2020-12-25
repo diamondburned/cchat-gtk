e159b0d611396ee470a244a45fbf0915cd2fd5d3
diamondburned <datutbrus@gmail.com>
2020-12-25 00:32:44 -0800

    removed dead code

--- internal/gts/httputil/nopwriter.go
+++
@@ -1,8 +0,0 @@
-package httputil
-
-type nopWriterImpl struct{}
-
-var nopWriter = nopWriterImpl{}
-
-func (nopWriterImpl) Write(b []byte) (int, error) { return len(b), nil }
-func (nopWriterImpl) Close() error                { return nil }
